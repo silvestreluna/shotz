ac15cf753dcde36473f13305db1b810de40a9320
Silvestre Luna <slvtrluna@gmail.com>
2019-04-20 11:45:51 -0500

    data

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,14 @@
+
+import movies from './components/movies/movies';
+import locations from './components/locations/locations';
+
+
 import '../styles/main.scss';
 
-console.error('hi');
+
+const init = () => {
+  movies.initializeMovies();
+  locations.initializeLocations();
+};
+
+init();
