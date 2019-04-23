8323881f4179a1c729727af5dc8c76a7a4fd6241
Silvestre Luna <slvtrluna@gmail.com>
2019-04-22 19:16:03 -0500

    locations card

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -8,9 +8,15 @@ let locations = [];
 
 const domStringBuilder = () => {
   let domString = '';
+  domString += '<div class="text-center row">';
   locations.forEach((location) => {
+    domString += `<div id=${location.id} class="card col-2">`;
     domString += `<h3>${location.name}</h3>`;
+    domString += `<img class="card-img-top " src=${location.imageUrl} alt="location" />`;
+    domString += `<p>${location.address}</p>`;
+    domString += '</div>';
   });
+  domString += '</div>';
   util.printToDom('locations', domString);
 };
 
